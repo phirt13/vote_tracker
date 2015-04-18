af831e1d7cf9fd9bb057ef615df3735c65c13125
Philip A. Hirt <phirt13@gmail.com>
2015-04-17 17:21:05 -0700

    convert server to listen user-specified port number || 500

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 5000;
+
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + "/public"));
 
@@ -16,28 +17,6 @@ app.get('*', function(req, res){
   res.sendFile(__dirname + '/public/404.html');
 });
 
-app.listen(port, function() {
-  console.log("server starting. available at http://localhost:" + port);
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
 });
-// 'use strict';
-
-// var express = require('express');
-// var app = express();
-
-// app.use(express.static(__dirname + '/public'));
-
-// app.get('/secret', function(request, response) {
-//   response.status(200).send('<h1>Hello Stranger!</h1>');
-// });
-
-// app.get('/greet/:person', function(req, res) {
-//   respose.send('Well done! You found the secret. Now tell nobody...')
-// });
-
-// app.get('/*', function(req, res) {
-//   res.status(404).send('could not find page');
-// });
-
-// app.listen(process.env.PORT || 3000, function() {
-//   console.log('server running');
-// });
